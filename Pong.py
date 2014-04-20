ee5f0880451f7571176b79253bdf8aa1bcff3aca
eugeneyan <eugeneyanziyou@gmail.com>
2014-04-20 21:54:36 +0800

    updated Pong

--- Pong.py
+++ Pong.py
@@ -45,7 +45,7 @@ def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel, ball_pos, ball_vel, player1_score, player2_score
         
     # draw mid line and gutters
-    canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
+    canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "white")
     canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
     canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
     
@@ -59,8 +59,8 @@ def draw(canvas):
     # left gutter test
     if ball_pos[0] <= BALL_RADIUS + PAD_WIDTH:
         if paddle1_pos - HALF_PAD_HEIGHT < ball_pos[1] < paddle1_pos + HALF_PAD_HEIGHT:
-            ball_vel[0] *= 1.1
-            ball_vel[0] = -ball_vel[0]
+            ball_vel[1] *= 1.1
+            ball_vel[0] = -ball_vel[0] * 1.1
         else:
             player2_score += 1
             spawn_ball(RIGHT)
@@ -68,8 +68,8 @@ def draw(canvas):
     # right gutter test
     if ball_pos[0] >= (WIDTH - 1) - (BALL_RADIUS + PAD_WIDTH):
         if paddle2_pos - HALF_PAD_HEIGHT < ball_pos[1] < paddle2_pos + HALF_PAD_HEIGHT:
-            ball_vel[0] *= 1.1
-            ball_vel[0] = -ball_vel[0]
+            ball_vel[1] *= 1.1
+            ball_vel[0] = -ball_vel[0] * 1.1
         else:
             player1_score += 1
             spawn_ball(LEFT)
@@ -80,18 +80,20 @@ def draw(canvas):
     ball_pos[1] += ball_vel[1]
     
     # update paddle's vertical position, keep paddle on the screen
-    if paddle1_pos + paddle1_vel >= 0 + HALF_PAD_HEIGHT and paddle1_pos + paddle1_vel <= HEIGHT - 1 - HALF_PAD_HEIGHT:
+    if paddle1_pos + paddle1_vel >= 0 + HALF_PAD_HEIGHT and paddle1_pos + paddle1_vel <= HEIGHT - HALF_PAD_HEIGHT:
         paddle1_pos += paddle1_vel
-    if paddle2_pos + paddle2_vel >= 0 + HALF_PAD_HEIGHT and paddle2_pos + paddle2_vel <= HEIGHT - 1 - HALF_PAD_HEIGHT:
+    if paddle2_pos + paddle2_vel >= 0 + HALF_PAD_HEIGHT and paddle2_pos + paddle2_vel <= HEIGHT - HALF_PAD_HEIGHT:
         paddle2_pos += paddle2_vel
+    #uncomment the line below and comment the two lines above to play with a computer
+        #paddle2_pos = ball_pos[1]
     
     # draw paddles
     canvas.draw_polygon([(0, paddle1_pos - HALF_PAD_HEIGHT), (0, paddle1_pos + HALF_PAD_HEIGHT), (PAD_WIDTH, paddle1_pos + HALF_PAD_HEIGHT), (PAD_WIDTH, paddle1_pos - HALF_PAD_HEIGHT)], 1, "white", "white")  
-    canvas.draw_polygon([(WIDTH - 1, paddle2_pos - HALF_PAD_HEIGHT), ((WIDTH - 1), paddle2_pos + HALF_PAD_HEIGHT), ((WIDTH - 1 - PAD_WIDTH), paddle2_pos + HALF_PAD_HEIGHT), ((WIDTH - 1 - PAD_WIDTH), paddle2_pos - HALF_PAD_HEIGHT)], 1, "white", "white")
+    canvas.draw_polygon([(WIDTH, paddle2_pos - HALF_PAD_HEIGHT), (WIDTH, paddle2_pos + HALF_PAD_HEIGHT), ((WIDTH - PAD_WIDTH), paddle2_pos + HALF_PAD_HEIGHT), ((WIDTH - PAD_WIDTH), paddle2_pos - HALF_PAD_HEIGHT)], 1, "white", "white")
     
     # draw scores
-    canvas.draw_text(str(player1_score), (150, 100), 20, "white")
-    canvas.draw_text(str(player2_score), (450, 100), 20, "white")
+    canvas.draw_text(str(player1_score), (150, 100), 40, "white", "sans-serif")
+    canvas.draw_text(str(player2_score), (450, 100), 40, "white", "sans-serif")
     
 def keydown(key):
     global paddle1_vel, paddle2_vel
